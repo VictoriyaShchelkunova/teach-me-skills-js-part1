2857a17a06c8473db9516a94f3a0965683d0c5d5
Виктория Щелкунова <smahito89@gmail.com>
2020-05-07 14:24:42 +0300

    hw1

--- src/hw-1.js
+++ src/hw-1.js
@@ -4,35 +4,67 @@
  */
 function task1(num1, num2, str) {
   let sum = null;
-  // ваш код должен быть ниже этой строки
+  str = Number(str);
 
-  // ваш код должен быть выше этой строки
+  if (str || str === 0) {
+    sum = num1 + num2 + str;
+  } else {
+    sum = 'неверные данные';
+  }
   return sum;
 }
 
-/**
- * Код ниже перепишете с использованием switch
+console.log(task1(1, 2, '0'));
+console.log(task1(1, 2, 'am'));
 
-  let result = null;
 
-  if (age > 5) {
-    result = 'Он старше 5 лет';
-  } else if (age > 8) {
-    result = 'Он старше 8 лет';
-  } else if (age > 11) {
-    result = 'Он старше 11 лет';
-  } else {
-    result = 'Нет подходящего возраста';
-  }
+/*Код ниже перепишете с использованием switch
 
- */
+let result = null;
+
+if (age > 5) {
+  result = 'Он старше 5 лет';
+} else if (age > 8) {
+  result = 'Он старше 8 лет';
+} else if (age > 11) {
+  result = 'Он старше 11 лет';
+} else {
+  result = 'Нет подходящего возраста';
+}*/
+
 function task2(age) {
   let result = null;
-  // ваш код должен быть ниже этой строки
+  switch (age) {
+    case 5:
+    case 6:
+    case 7:
+    case 8: {
+      result = 'Он старше 5 лет';
+      break;
+    }
+    case 9:
+    case 10:
+    case 11: {
+      result = 'Он старше 8 лет';
+      break;
+    }
+    case 12:
+    case 13:
+    case 14:
+    case 15: {
+      result = 'Он старше 11 лет';
+      break;
+    }
+    default: {
+      result = 'Нет подходящего возраста';
+      break;
+    }
 
-  // ваш код должен быть выше этой строки
+  }
   return result;
 }
+
+console.log(task2(6));
 
 /**
  * Используя while напишите цикл от 0 до 21 и каждое нечетное число суммируйте к переменной result
@@ -40,11 +72,16 @@ function task2(age) {
  */
 function task3() {
   let result = 0;
-  // ваш код должен быть ниже этой строки
-
-  // ваш код должен быть выше этой строки
+  let count = 0;
+  while (count < 21) {
+    if (count % 2) {
+      result += count;
+    }
+    count++;
+  }
   return result;
 }
+console.log(task3());
 
 /**
  * Используя for напишите цикл от 0 до 100. На каждой итерации, что делится на 5 без остатка
@@ -52,20 +89,36 @@ function task3() {
  *
  */
 function task4() {
-  // ваш код должен быть ниже этой строки
-
-  // ваш код должен быть выше этой строки
+  for (let count = 0; count < 100; count++) {
+    if (count % 5 === 0) {
+      console.log(5);
+    }
+  }
 }
+task4();
 
 /**
  * В функцию приходят 4 аргумента все разных типов, какие бы не были значения результат всегда должен быть один и тот же, true
  *
  */
 function task5(num, str, bool, val) {
+  num = Boolean(num);
+  str = Boolean(str);
+  bool = Boolean(bool);
+  val = Boolean(val);
+
   let result = num || str || bool || val; // выражение с || менять нельзя, остальное в ваших руках
+
+  if (num == false && str == false && bool == false && val == false) {
+    result = true;
+  }
 
   return result;
 }
+
+console.log(task5(3, '', false, 'hello'));
+console.log(task5(0, '', false, undefined));
+
 
 /**
  * Вы должны дополнить код так, чтобы переменная result была не больше number
@@ -75,16 +128,21 @@ function task6(number) {
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-      // ваш код должен быть ниже этой строки
 
-      result += i + j;
-
-      // ваш код должен быть выше этой строки
+      if (result < number) {
+        result += i + j;
+        if (result >= number) {
+          result -= (i + j);
+        }
+      }
     }
   }
 
   return result;
 }
+
+console.log(task6(18));
+console.log(task6(32))
 
 
 /**
@@ -94,11 +152,15 @@ function task6(number) {
  */
 function task7() {
   let result = null;
-  // ваш код должен быть ниже этой строки
-
-  // ваш код должен быть выше этой строки
+  result = '';
+  let name = 'Victoriya';
+  for (let i = 0; i < name.length; i++) {
+    result += name[i];
+  }
   return result;
 }
+
+console.log(task7());
 
 
 /**
@@ -107,9 +169,17 @@ function task7() {
  */
 function task8(num) {
   let str = '10n';
+  let strRes = '';
   let result = null;
-  // ваш код должен быть ниже этой строки
-
-  // ваш код должен быть выше этой строки
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (Number(letter) || letter === '0') {
+      strRes += letter;
+    }
+  }
+  str = Number(strRes);
+  result = num / str;
   return result;
 }
+
+console.log(task8(20));
