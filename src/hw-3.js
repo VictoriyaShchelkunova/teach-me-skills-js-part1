7f02c3b5ac07c9f871dfd837666d294f15c7000f
Виктория Щелкунова <smahito89@gmail.com>
2020-05-18 16:30:19 +0300

    hm-3 done

--- src/hw-3.js
+++ src/hw-3.js
@@ -8,7 +8,17 @@
  */
 function task1(str) {
 
+  let arrFromStr = [...str];
+  for (let i = 0; i < arrFromStr.length; i++) {
+    for (let j = i + 1; j < arrFromStr.length; j++) {
+      if (arrFromStr[i] === arrFromStr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
+console.log(task1('Aaret64ip'));
 
 /**
  * Проверить строку str на буквы, если в строке есть буквы, то их нужно вынести в переменную result и вернуть эту
@@ -16,28 +26,43 @@ function task1(str) {
  */
 function task2(str) {
   let result = '';
-
+  let arrFromStr = [...str];
+  arrFromStr.forEach(letter => {
+    if (isNaN(letter)) {
+      result += letter;
+    }
+  })
 
   return result;
 }
+console.log(task2('ab2cd1'));
 
 /**
  * arr массив чисел. Нужно найти в массиве найбольшое число и вернуть его
  */
 function task3(arr) {
-
+  return Math.max(...arr);
 }
+console.log(task3([1, 23, 345, 14]));
 
 /**
  * В функцию приходит объект obj. Нужно скопировать все его свойства в объект result и вернуть его
  */
 function task4(obj) {
-  const result = {};
-  // ваш код должен быть ниже этой строки
+  const result = JSON.parse(JSON.stringify(obj));
 
-  // ваш код должен быть выше этой строки
   return result;
 }
+
+console.log(task4({
+  name: 'Mike',
+  surname: 'Smith',
+  age: 25,
+  address: {
+    country: 'USA',
+    city: 'Boston'
+  }
+}))
 
 /**
  * Приходит строка str такого вида "Ivan Ivanov", вы должны сохранить первые символы в переменную result. Должен
@@ -45,20 +70,39 @@ function task4(obj) {
  * Еще пример: str => "Vitalik Skopets". result => "V.S."
  */
 function task5(str) {
-
+  let arrFromStr = str.split(' ');
+  return (`${arrFromStr[0][0]}.${arrFromStr[1][0]}.`);
 }
+console.log(task5("Ivan Ivanov"));
+console.log(task5("Vitalik Skopets"));
+
 
 /**
  * Написать логику, которая будет возвращать название планеты. По ее номеру начиная от ближайшей к солнцу.
  * Примечание: написать эту логику без if и switch!!!!!!!
  */
 function task6(num) {
-
+  let planets = {
+    1: 'Mercury',
+    2: 'Venus',
+    3: 'Earth',
+    4: 'Mars',
+    5: 'Jupiter',
+    6: 'Saturn',
+    7: 'Uranus',
+    8: 'Neptune',
+    9: 'Pluto'
+  }
+  return planets[num];
 }
+console.log(task6(6));
+
+
 
 /**
  * Функция должна удалять первый и последний символы из строки str и вернуть получившеюся строку.
  */
 function task7(str) {
-
+  return str.slice(1, str.length - 1);
 }
+console.log(task7('hello'));
