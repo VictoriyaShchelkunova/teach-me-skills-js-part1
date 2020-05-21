8faf80850008dc4a0e16ecceac5e078cf661585b
Виктория Щелкунова <smahito89@gmail.com>
2020-05-21 15:18:06 +0300

    hw-4

---
+++ src/hw-4.js
@@ -0,0 +1,51 @@
+// без использования рекурсии;
+let arr = [];
+
+let a = 0;
+arr.push(a);
+
+let b = 1;
+arr.push(b);
+
+let c = 0;
+
+let i = 0;
+
+function getNumbersFibonache(a, b, interval) {
+
+    while (i < interval) {
+        c = a + b;
+        arr.push(c);
+        a = b;
+        b = c;
+        i++;
+    }
+    return arr;
+}
+console.log(getNumbersFibonache(a, b, 10));
+
+// с использованием рекурсии;
+
+let arrNumbers = [];
+
+let firstNum = 0;
+arrNumbers.push(firstNum);
+
+let secondNum = 1;
+arrNumbers.push(secondNum);
+
+let sum = 0;
+
+let counter = 0;
+
+function getArrayOfFibonache(firstNum, secondNum, interval) {
+    sum = firstNum + secondNum;
+    arrNumbers.push(sum);
+    counter++;
+    while (counter < interval) {
+        getArrayOfFibonache(secondNum, sum, interval);
+    }
+    return arrNumbers;
+}
+
+console.log(getArrayOfFibonache(firstNum, secondNum, 10));
