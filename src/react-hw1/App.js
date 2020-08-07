8e11260cca575d970513d16c99b84b9f56747a7e
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2020-08-07 23:32:50 +0300

    redon react-hw1

--- src/react-hw1/App.js
+++ src/react-hw1/App.js
@@ -5,35 +5,37 @@ export class App extends Component {
     constructor (props){
         super(props)
         this.state = {
-            title: ''
+            title: '',
+            inputValue: ''
         }
     }
     
     inputHandler = ({target}) => {
         this.setState((prevState) => {
             return {
-                title: target.value
+                inputValue: target.value
             }
         })
     }
 
     handlerSave = () => {
-        const p = document.getElementsByTagName('p')[0];
-        p.innerText = this.state.title;
-        const input = document.getElementsByTagName('input')[0];
-        input.value = ''
+        this.setState({
+            title: this.state.inputValue,
+            inputValue: ''
+        })
     }
 
     handlerDelete = () => {
-        const p = document.getElementsByTagName('p')[0];
-        p.innerText = ''
+        this.setState({
+            title: '',
+        })
     }
 
     render(){
         return (
             <>
-            <p></p>
-            <input type="text" onChange={this.inputHandler}/>
+            <p>{this.state.title}</p>
+            <input type="text" onChange={this.inputHandler} value={this.state.inputValue}/>
             <button onClick={this.handlerSave}>Save</button>
             <button onClick={this.handlerDelete}>Delete</button>
             </>
